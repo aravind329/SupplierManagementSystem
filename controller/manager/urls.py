4decf7c95a17cf4730df679508ab1462e6428df9
MATAM ARAVIND <73662879+aravind329@users.noreply.github.com>
2022-07-21 17:24:25 +0530

    Delete controller directory

--- controller/manager/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ManagerListView, ManagerView, TopSellerView
-
-urlpatterns = [
-    path('', ManagerListView.as_view()),
-    path('topseller', TopSellerView.as_view()),
-    path('<pk>', ManagerView.as_view()),
-]
